f56b5dc35d3b92ea7a043d164311fa6fd26299ff
Gustavo Tenorio <gustavo.tenorio@avanade.com>
2019-08-10 21:39:47 -0300

    Save new developers and start the like function.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,10 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({
-        message: `Hello my ${req.query.name}`
-    });
-});
-
-routes.post('/devs', (req, res) => {
-    return res.json({
-        ok: true
-    });
-
-});
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
 
 module.exports = routes;
